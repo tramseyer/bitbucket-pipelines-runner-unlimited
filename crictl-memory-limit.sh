c5664f28c4c6efcc8070e1d2def21b94b242e14e
t <t@ramseyer.email>
2024-07-28 14:03:11 +0000

    Iterate trough existing containers via loop

--- crictl-memory-limit.sh
+++ crictl-memory-limit.sh
@@ -3,13 +3,16 @@ while true
 do
     crictl ps -a | grep -E "Running\s+runner\s" | cut -d " " -f 1 | xargs -I {} crictl exec {} /bin/sh -c '
         MEMORY=$((1024 * 1024 * 1024 * 1024)) # 1TB
-        OUTPUT=$(./docker inspect --format="{{.Id}} {{.Name}} {{.HostConfig.Memory}} {{.HostConfig.MemorySwap}}" $(./docker ps -aq) 2> /dev/null | grep ".*_build" | grep -v "_build\s$MEMORY")
-        if [ $? -eq 0 ]; then
-            echo "Found *_build container with different memory limit: $OUTPUT"
-            ID="${OUTPUT%% *}"
-            ./docker update --memory="$MEMORY" --memory-swap="$MEMORY" "$ID"
-            ./docker inspect --format "{{.Id}} {{.HostConfig.Memory}} {{.HostConfig.MemorySwap}}" "$ID"
-        fi
+        for CONTAINER in $(./docker ps -aq)
+        do
+            OUTPUT=$(./docker inspect --format="{{.Id}} {{.Name}} {{.HostConfig.Memory}} {{.HostConfig.MemorySwap}}" "$CONTAINER" | grep ".*_build" | grep -v "_build\s$MEMORY")
+            if [ $? -eq 0 ]; then
+                echo "Found *_build container with different memory limit: $OUTPUT"
+                ID="${OUTPUT%% *}"
+                ./docker update --memory="$MEMORY" --memory-swap="$MEMORY" "$ID"
+                ./docker inspect --format "{{.Id}} {{.HostConfig.Memory}} {{.HostConfig.MemorySwap}}" "$ID"
+            fi
+        done
     '
     sleep 30
 done
--- docker-cpu-quota.sh
+++ docker-cpu-quota.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 while true
 do
-    OUTPUT=$(docker inspect --format="{{.Id}} {{.Name}} {{.HostConfig.CpuQuota}}" $(docker ps -aq) | grep ".*_build\s400000")
-    if [ $? -eq 0 ]; then
-        echo "Found *_build container with active CPU quota: $OUTPUT"
-        ID="${OUTPUT%% *}"
-        docker update --cpu-quota=-1 "$ID"
-        docker inspect --format "{{.Id}} {{.HostConfig.CpuQuota}}" "$ID"
-    fi
+    for CONTAINER in $(docker ps -aq)
+    do
+        OUTPUT=$(docker inspect --format="{{.Id}} {{.Name}} {{.HostConfig.CpuQuota}}" "$CONTAINER" | grep ".*_build\s400000")
+        if [ $? -eq 0 ]; then
+            echo "Found *_build container with active CPU quota: $OUTPUT"
+            ID="${OUTPUT%% *}"
+            docker update --cpu-quota=-1 "$ID"
+            docker inspect --format "{{.Id}} {{.HostConfig.CpuQuota}}" "$ID"
+        fi
+    done
     sleep 10
 done
